657ae91c5c2e562836b780c17159ab14745b34aa
FranB2MIL <franciscojbollea@gmail.com>
2026-06-22 22:37:35 -0300

    fixed navigate

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,23 @@
+import { useNavigate } from "react-router-dom"
+
 function Header() {
-  return (
-    <header className="header">
-      <div className="header-content">
-        <img src="/ghost2.png" alt="Ghost" className="header-ghost" />
-        <div>
-          <h1 className="header-title">Grimoires of Sol</h1>
-          <p className="header-subtitle">Ask anything about the lore</p>
-        </div>
-      </div>
-       <button className="header-exit-btn" onClick={() => navigate("/")}>
-        ✕ Exit
-      </button>
-    </header>
-  )
+    const navigate = useNavigate()
+
+    return (
+
+        <header className="header">
+            <div className="header-content">
+                <img src="/ghost2.png" alt="Ghost" className="header-ghost" />
+                <div>
+                    <h1 className="header-title">Grimoires of Sol</h1>
+                    <p className="header-subtitle">Ask anything about the lore</p>
+                </div>
+            </div>
+            <button className="header-exit-btn" onClick={() => navigate("/")}>
+                ✕ Exit
+            </button>
+        </header>
+    )
 }
 
 export default Header
